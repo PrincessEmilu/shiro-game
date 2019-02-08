2cf7e8f8b370cfe4bb923f89d52343a1efff4701
brs7770 <brs7770@IGM2350-07>
2019-02-08 17:45:24 -0500

    Added the rest of the classes

---
+++ Shiro/Shiro/Boss.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shiro
+{
+    class Boss : Battle
+    {
+        //Fields
+
+        //Constructor
+
+        //Properties
+
+        //Methods
+    }
+}
