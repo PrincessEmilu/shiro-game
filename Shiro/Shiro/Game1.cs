8a4c2f235d3d2ba4e220aca7307f14e7b5f9c817
brs7770 <brs7770@IGM2350-07>
2019-02-22 18:04:42 -0500

    Fixed merge

--- Shiro/Shiro/Game1.cs
+++ Shiro/Shiro/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Shiro
 {
@@ -24,11 +25,25 @@ namespace Shiro
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         GameState state;
+        Texture2D background;
+        Texture2D testCat;
+        Texture2D enemyCat;
+
+        private int viewportMoveX;
+        private int viewportMoveY;
+        public int width;
+        public int height;
+
+        private Player player;
+        private Enemy enemy;
+
+        public Random rng;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            rng = new Random();
         }
 
         /// <summary>
@@ -57,6 +72,18 @@ namespace Shiro
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            testCat = Content.Load<Texture2D>("cat");
+            enemyCat = Content.Load<Texture2D>("enemy cat");
+            background = Content.Load<Texture2D>("cat");
+
+            width = graphics.GraphicsDevice.Viewport.Width;
+            height = graphics.GraphicsDevice.Viewport.Height;
+
+            Rectangle pos = new Rectangle(50, 50, 50, 50);
+            Rectangle pos2 = new Rectangle(100, 100, 50, 50);
+
+            player = new Player(testCat, pos, width, height);
+            enemy = new Enemy(enemyCat, pos2, width, height, rng);
         }
 
         /// <summary>
@@ -99,6 +126,27 @@ namespace Shiro
                     break;
             }
 
+            player.Update(gameTime);
+
+            KeyboardState kbState = Keyboard.GetState();
+            /*if (kbState.IsKeyDown(Keys.Up))
+            {                
+                graphics.GraphicsDevice.Viewport = new Viewport(0, viewportMoveY -= 1, width, height);                
+            }
+            if (kbState.IsKeyDown(Keys.Down))
+            {
+                graphics.GraphicsDevice.Viewport = new Viewport(0, viewportMoveY += 1, width, height);
+            }
+            if (kbState.IsKeyDown(Keys.Left))
+            {
+                graphics.GraphicsDevice.Viewport = new Viewport(viewportMoveX -= 1, 0, width, height);
+            }
+            if (kbState.IsKeyDown(Keys.Right))
+            {                
+                graphics.GraphicsDevice.Viewport = new Viewport(viewportMoveX += 1, 0, width, height);
+            }*/
+
+
             base.Update(gameTime);
         }
 
@@ -130,6 +178,15 @@ namespace Shiro
                 default:
                     break;
             }
+
+            spriteBatch.Begin();
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.Draw(background, new Rectangle(100, 100, 100, 100), Color.White);
+
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
