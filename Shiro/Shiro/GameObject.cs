c211e28fd7ba9538d2f5a6ca34b5007305b88102
brs7770 <brs7770@IGM2350-07>
2019-02-22 16:31:44 -0500

    GameObject created

--- Shiro/Shiro/GameObject.cs
+++ Shiro/Shiro/GameObject.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Shiro
 {
     abstract class GameObject
     {
-        //Fields
+        //Fields, a texture for the object and rectangle for the position of the object
+        protected Texture2D texture;
+        protected Rectangle position;
 
-        //Constructor
+        //Constructor that takes two parameters
+        protected GameObject(Texture2D texture, Rectangle position)
+        {
+            this.texture = texture;
+            this.position = position;
+        }
 
-        //Properties
+        //Property for the position field
+        public Rectangle Position
+        {
+            get { return position; }
+        }
 
-        //Methods
+        //Draw method
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, position, Color.White);
+        }
+
+        public abstract void Update(GameTime gameTime);
     }
 }
