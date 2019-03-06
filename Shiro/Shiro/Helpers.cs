7067040ba483586e8281792e87cc5703f81aa4ff
eh8582 <eh8582@rit.edu>
2019-03-06 15:35:10 -0500

    Attacks can now be blocked; effect player and enemy stamina

---
+++ Shiro/Shiro/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Shiro
+{
+    static class Helpers
+    {
+        //Single key press- return if the key was simply a single key press instead of currently being down
+        static public bool SingleKeyPress(Keys key, KeyboardState pbState, KeyboardState kbState)
+        {
+            if (kbState.IsKeyDown(key) && (pbState.IsKeyUp(key) || pbState == null))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
