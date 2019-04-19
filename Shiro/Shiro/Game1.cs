5178f20b2eeea33e3b786c8e1bb55e02e83ecf5b
Emily Horton <emilyhorton1994@gmail.com>
2019-04-19 14:05:46 -0400

    ?

--- Shiro/Shiro/Game1.cs
+++ Shiro/Shiro/Game1.cs
@@ -132,7 +132,6 @@ namespace Shiro
             Content.RootDirectory = "Content";
         }
 
-
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
